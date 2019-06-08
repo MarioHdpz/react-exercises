8821c21b28721dcbfe2c5f51f97368c222e3ede5
Mario HD <mariohdpz@gmail.com>
2019-06-07 21:19:46 -0500

    Add tips to exercise 2

--- src/components/RealTimeSum/index.js
+++ src/components/RealTimeSum/index.js
@@ -5,22 +5,34 @@ export default class Counter extends Component {
         count: [0, 0]
     }
 
-    add = index => {}
+    add = index => {
+        const newState = this.state.slice(); // This line only creates a copy
+        /**
+         * Here you can modify newState
+         */
+        this.setState({state: newState})
+    }
 
-    substract = index => {}
+    substract = index => {
+        const newState = this.state.slice(); // This line only creates a copy
+        /**
+         * Here you can modify newState
+         */
+        this.setState({state: newState})
+    }
 
     render() {
         return (
             <div>
                 <div className="counter">
-                    <div>-</div>
+                    <div onClick={() => {}}>-</div>
                     <h1>{this.state.count[0]}</h1>
-                    <div>+</div>
+                    <div onClick={() => {}}>+</div>
                 </div>
                 <div className="counter">
-                    <div>-</div>
+                    <div onClick={() => {}}>-</div>
                     <h1>{this.state.count[1]}</h1>
-                    <div>+</div>
+                    <div onClick={() => {}}>+</div>
                 </div>
                 <h1>Sum: {0}</h1>
             </div>
