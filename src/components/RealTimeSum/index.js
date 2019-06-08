3ae14573ad55d2bc39643f068a9d0ce70db56628
Mario HD <mariohdpz@gmail.com>
2019-06-07 21:53:32 -0500

    Solution 2

--- src/components/RealTimeSum/index.js
+++ src/components/RealTimeSum/index.js
@@ -6,36 +6,33 @@ export default class Counter extends Component {
     }
 
     add = index => {
-        const newState = this.state.slice(); // This line only creates a copy
-        /**
-         * Here you can modify newState
-         */
-        this.setState({state: newState})
+        const newCount = this.state.count.slice(); // This line only creates a copy of count
+        newCount[index] = this.state.count[index] + 1;
+        this.setState({count: newCount});
     }
 
     substract = index => {
-        const newState = this.state.slice(); // This line only creates a copy
-        /**
-         * Here you can modify newState
-         */
-        this.setState({state: newState})
+        const newCount = this.state.count.slice(); // This line only creates a copy of count
+        newCount[index] = this.state.count[index] - 1;
+        this.setState({count: newCount});
     }
 
     render() {
         return (
             <div>
                 <div className="counter">
-                    <div onClick={() => {}}>-</div>
+                    <div onClick={() => this.substract(0)}>-</div>
                     <h1>{this.state.count[0]}</h1>
-                    <div onClick={() => {}}>+</div>
+                    <div onClick={() => this.add(0)}>+</div>
                 </div>
                 <div className="counter">
-                    <div onClick={() => {}}>-</div>
+                    <div onClick={() => this.substract(1)}>-</div>
                     <h1>{this.state.count[1]}</h1>
-                    <div onClick={() => {}}>+</div>
+                    <div onClick={() => this.add(1)}>+</div>
                 </div>
-                <h1>Sum: {0}</h1>
+                <h1>Sum: {this.state.count[0] + this.state.count[1]}</h1>
+                <h1>Difference: {Math.abs(this.state.count[0] - this.state.count[1])}</h1>
             </div>
         );
-    }0
+    }
 }
